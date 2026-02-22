370ef7fe3bcacb955b3d547d8b6b170ca838626b
Antonio Quinonez <antquinonez@gmail.com>
2026-02-21 22:03:08 -0800

    Add ConversationHistory to __all__ exports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,12 @@ from .FFAI import FFAI
 from .FFAIClientBase import FFAIClientBase
 from .OrderedPromptHistory import OrderedPromptHistory
 from .PermanentHistory import PermanentHistory
+from .ConversationHistory import ConversationHistory
 
-__all__ = ["FFAI", "FFAIClientBase", "OrderedPromptHistory", "PermanentHistory"]
+__all__ = [
+    "FFAI",
+    "FFAIClientBase",
+    "OrderedPromptHistory",
+    "PermanentHistory",
+    "ConversationHistory",
+]
